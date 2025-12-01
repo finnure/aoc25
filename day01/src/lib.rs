48f5d8edefc26a94ff03076ed75874b11d048a88
Finnur Eiríksson <finnur.eiriksson@advania.is>
2025-12-01 13:51:09 +0000

    day 1 part 2

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn solve(input: &str) {
     part1(input);
+    part2(input);
 }
 
 fn part1(input: &str) {
@@ -28,4 +29,37 @@ fn part1(input: &str) {
         }
     }
     println!("Part 1: {}", countzero);
+}
+
+fn part2(input: &str) {
+    let mut pos = 50;
+    let mut countzero = 0;
+    for line in input.lines() {
+        let (dir, dist) = line.split_at(1);
+        let dist: i32 = dist.parse().unwrap();
+        match dir {
+            "L" => {
+                if pos == 0 {
+                    countzero -= 1;
+                }
+                pos -= dist;
+                while pos < 0 {
+                    pos += 100;
+                    countzero += 1;
+                }
+                if pos == 0 {
+                    countzero += 1;
+                }
+            }
+            "R" => {
+                pos += dist;
+                while pos >= 100 {
+                    pos -= 100;
+                    countzero += 1;
+                }
+            }
+            _ => panic!("Invalid direction"),
+        }
+    }
+    println!("Part 2: {}", countzero);
 }
