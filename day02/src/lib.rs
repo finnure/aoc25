a37f33b4572834c8c6a1c796a045fb2001c7730a
Finnur Eiríksson <finnur.eiriksson@advania.is>
2025-12-02 18:06:31 +0000

    day 2 part 1

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,3 +1,25 @@
 pub fn solve(input: &str) {
-    println!("got input {}", input);
+    println!("part 1: {}", part1(input));
+}
+
+fn part1(input: &str) -> u64 {
+    let mut sum: u64 = 0;
+    for range in input.trim().split(',') {
+        let (left, right) = range.split_once('-').unwrap();
+        let left: u64 = left.parse().unwrap();
+        let right: u64 = right.parse().unwrap();
+        for id in left..=right {
+            // convert id to string, check if it has even number of digits and check if first half is same as second half
+            let id_str = id.to_string();
+            let len = id_str.len();
+            if len % 2 != 0 {
+                continue;
+            }
+            let half = len / 2;
+            if &id_str[..half] == &id_str[half..] {
+                sum += id;
+            }
+        }
+    }
+    sum
 }
