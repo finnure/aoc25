6fff88ddca73a03e04ed7646233a9af2684a7b2e
Finnur Eiríksson <finnur.eiriksson@advania.is>
2025-12-04 22:41:40 +0000

    day 4 part 1

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -3,12 +3,61 @@ pub fn solve(input: &str) {
     println!("part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> i32 {
-    0
+fn part1(input: &str) -> i32 {
+    let lines: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect();
+    let mut sum = 0;
+    for line in 0..lines.len() {
+        for col in 0..lines[line].len() {
+            let c = lines[line][col];
+            match c {
+                '@' => {
+                    if count_adjacent(&lines, line, col) < 4 {
+                        sum += 1
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+    sum
 }
 
 fn part2(_input: &str) -> i32 {
     0
+}
+
+fn count_adjacent(lines: &Vec<Vec<char>>, line: usize, col: usize) -> i32 {
+    let mut count = 0;
+    let directions = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+    for (dl, dc) in directions.iter() {
+        let new_line = line as isize + dl;
+        let new_col = col as isize + dc;
+        if new_line >= 0
+            && new_line < lines.len() as isize
+            && new_col >= 0
+            && new_col < lines[0].len() as isize
+        {
+            if lines[new_line as usize][new_col as usize] == '@' {
+                count += 1;
+                if count >= 4 {
+                    return count;
+                }
+            }
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -18,21 +67,33 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = "\
-987654321111111
-811111111111119
-234234234234278
-818181911112111";
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
         let result = part1(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 13);
     }
 
     #[test]
     fn test_part2() {
         let input = "\
-987654321111111
-811111111111119
-234234234234278
-818181911112111";
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
         let result = part2(input);
         assert_eq!(result, 0);
     }
