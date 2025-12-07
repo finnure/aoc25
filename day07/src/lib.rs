a146f46b8928fe3aad4ff0f30b40d4384f68fce2
Finnur Eiríksson <finnur.eiriksson@advania.is>
2025-12-07 15:23:19 +0000

    day 7 part 1

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,10 +1,38 @@
+use std::collections::HashMap;
+
 pub fn solve(input: &str) {
     println!("part 1: {}", part1(input));
     println!("part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> i32 {
-    0
+fn part1(input: &str) -> i32 {
+    let mut result = 0;
+    let mut running: HashMap<usize, bool> = HashMap::new();
+    let lines = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    for line in lines {
+        for i in 0..line.len() {
+            match line[i] {
+                'S' => {
+                    running.insert(i, true);
+                }
+                '^' => {
+                    if let Some(is_running) = running.get(&i) {
+                        if *is_running {
+                            result += 1;
+                            running.insert(i, false);
+                            running.insert(i - 1, true);
+                            running.insert(i + 1, true);
+                        }
+                    }
+                }
+                _ => { /* do nothing */ }
+            }
+        }
+    }
+    result
 }
 
 fn part2(_input: &str) -> i32 {
@@ -18,22 +46,46 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = "\
-987654321111111
-811111111111119
-234234234234278
-818181911112111";
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
         let result = part1(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 21);
     }
 
     #[test]
     fn test_part2() {
         let input = "\
-987654321111111
-811111111111119
-234234234234278
-818181911112111";
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
         let result = part2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 40);
     }
 }
